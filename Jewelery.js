9a4a8e28164e122aa5c938436cd37805b95d6e36
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-07-10 12:04:00 +0000

    feat: Add inital screen files for Blog, Clothing,Electronic,Jewelry and Locations page.

---
+++ Jewelery.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Jewelery = () => {
+  return (
+    <View>
+      <Text>Jewelery</Text>
+    </View>
+  )
+}
+
+export default Jewelery
